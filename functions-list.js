7c8ed09a300a8d06c6cb67a591f810009faf4954
Etai Gross <etai@wix.com>
2015-03-25 18:33:57 +0200

    Create functions-list.js
    just a list of all css functions

---
+++ functions-list.js
@@ -0,0 +1,3 @@
+(function(){
+   return ["annotation", "attr", "blur", "brightness", "calc", "circle", "contrast", "element", "ellipse", "grayscale", "hsl", "hsla", "image", "inset", "invert", "matrix", "minmax", "opacity", "ornaments", "perspective", "polygon", "rect", "repeat", "rgb", "rgba", "rotate", "rotatex", "rotatey", "rotatez", "saturate", "scale", "scalex", "scaley", "scalez", "sepia", "skew", "skewx", "skewy", "steps", "styleset", "stylistic", "swash", "symbols", "translate", "translatex", "translatey", "translatez", "url", "var"];
+})();
